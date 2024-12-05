188f77ba96c506eeebc0fef5f57b0166f2c2b472
RamiAlcune <ramialcune2001@gmail.com>
2024-12-05 11:44:49 +0300

    fix ts setup 3

--- dist/utils/createTokenUser.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createTokenUser = void 0;
-const createTokenUser = (user) => {
-    return { username: user.username, id: user.id, role: user.role };
-};
-exports.createTokenUser = createTokenUser;
