d68d14af582b6d0d795ab516f471b3e3d86a156b
RamiAlcune <ramialcune2001@gmail.com>
2024-12-18 18:19:56 +0300

    return all auth

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -3,17 +3,16 @@ import { createProduct, deleteProduct, getAllProducts, getSingleProduct, updateP
 import { authenticateUser, authorizePermissions } from "../middlewares/authorazition";
 const ProductRouter = express.Router();
 
-
 //GET
 ProductRouter.get("/", getAllProducts);
 ProductRouter.get("/:id", getSingleProduct);
 ProductRouter.get("/:id/reviews", getSingleProductReviews);
 //POST
-ProductRouter.post("/createProduct", authenticateUser as RequestHandler,authorizePermissions("user"), createProduct);
+ProductRouter.post("/createProduct", authenticateUser as RequestHandler, authorizePermissions("user"), createProduct);
 //UploadImage
-ProductRouter.post("/uploadImage", authenticateUser as RequestHandler,authorizePermissions("user"), uploadImage);
+ProductRouter.post("/uploadImage", authenticateUser as RequestHandler, authorizePermissions("user"), uploadImage);
 //PATCH
-ProductRouter.patch("/:id", authenticateUser as RequestHandler,authorizePermissions("user"), updateProduct);
+ProductRouter.patch("/:id", authenticateUser as RequestHandler, authorizePermissions("user"), updateProduct);
 //DELETE
-ProductRouter.delete("/:id", authenticateUser as RequestHandler,authorizePermissions("user"), deleteProduct);
+ProductRouter.delete("/:id", authenticateUser as RequestHandler, authorizePermissions("user"), deleteProduct);
 export default ProductRouter;
--- src/routes/usersRoute.ts
+++ src/routes/usersRoute.ts
@@ -1,28 +1,17 @@
 import express, { RequestHandler } from "express";
 import { authenticateUser, authorizePermissions } from "../middlewares/authorazition";
-import {
-  getAllUsers,
-  getUserByID,
-  updateUser,
-  showCurrentUser,
-  updateUserPassword,
-} from "../controllers/userController";
+import { getAllUsers, getUserByID, updateUser, showCurrentUser, updateUserPassword } from "../controllers/userController";
 
 const UsersRoute = express.Router();
 
 // Route for getting all users
-UsersRoute.get("/", getAllUsers);
+UsersRoute.get("/", authenticateUser as RequestHandler, authorizePermissions("user", "admin"), getAllUsers);
 UsersRoute.patch("/UpdateUser", authenticateUser as RequestHandler, authorizePermissions("user", "admin"), updateUser);
 // Route for showing the current user
 UsersRoute.get("/showMe", authenticateUser as RequestHandler, showCurrentUser);
 
 // Route for changing user password
-UsersRoute.post(
-  "/changePassword",
-  authenticateUser as RequestHandler,
-  authorizePermissions("user", "admin"),
-  updateUserPassword
-);
+UsersRoute.post("/changePassword", authenticateUser as RequestHandler, authorizePermissions("user", "admin"), updateUserPassword);
 
 // Route for getting a user by ID
 UsersRoute.get("/:id", authenticateUser as RequestHandler, authorizePermissions("user", "admin"), getUserByID);
